676509e59c5b3266f51010a4dc0797d614c71e31
Sushovan Karmakar <sushovan.karmakar@randstadrisesmart.com>
2020-11-17 19:25:38 +0530

    bootstrapping, adding controllers

---
+++ src/main/java/com/example/springsecurity/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.springsecurity.controller;
+
+import com.example.springsecurity.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Sushovan Karmakar"),
+            new Student(2, "Suvajit Dey"),
+            new Student(3, "Puja Pal")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exists"));
+    }
+}
