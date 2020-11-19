ef72a16e85b60d83c610a451867ef704c47ec1cc
Sushovan Karmakar <sushovan.karmakar@randstadrisesmart.com>
2020-11-19 19:15:38 +0530

    dummy commit

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -31,6 +31,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
